033c477e9aab028b7b2e5d6c9140462c01d70641
Inácio Fabrício Nunes de Sousa <inacio.fabricio.nunes@gmail.com>
2022-03-08 16:44:48 -0300

    Nova configuração o server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, './build')));
 app.set('port', process.env.PORT || 3000);
 
 const server = app.listen(app.get('port'), function() {
