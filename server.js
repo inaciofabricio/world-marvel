80c1871c0f0bbdd565d5798866494c6b05ca7f8f
Inácio Fabrício Nunes de Sousa <inacio.fabricio.nunes@gmail.com>
2022-03-08 17:06:42 -0300

    Organizando o README.md

--- server.js
+++ server.js
@@ -1,17 +1,3 @@
-// const express = require('express');
-// const { resolve } = require('path');
-// const app = express();
- 
-// app.use(
-//     express.static(
-//         resolve(__dirname, './build')
-//     )
-// );
-// app.set('port', process.env.PORT || 3000, (err) => {
-//     if(err) { return console.log(err) }
-
-//     console.log('Servidor ON!!!');
-// });
 const path = require('path');
 const express = require('express');
 
