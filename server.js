812b52af66e76119ac7b7b3c86db51c82e75000f
Inácio Fabrício Nunes de Sousa <inacio.fabricio.nunes@gmail.com>
2022-03-08 16:34:35 -0300

    Nova configuração o server.js

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const { resolve } = require('path');
-const app = express();
+// const express = require('express');
+// const { resolve } = require('path');
+// const app = express();
  
-app.use(
-    express.static(
-        resolve(__dirname, './build')
-    )
-);
-app.set('port', process.env.PORT || 3000, (err) => {
-    if(err) { return console.log(err) }
+// app.use(
+//     express.static(
+//         resolve(__dirname, './build')
+//     )
+// );
+// app.set('port', process.env.PORT || 3000, (err) => {
+//     if(err) { return console.log(err) }
 
-    console.log('Servidor ON!!!');
+//     console.log('Servidor ON!!!');
+// });
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+app.set('port', process.env.PORT || 3000);
+
+const server = app.listen(app.get('port'), function() {
+  console.log('listening on port ', server.address().port);
 });
